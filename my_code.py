db2218a38dd2ef97db9fb991fade406bf6f25048
yesak1 <23kkazliner@athenian.org>
2020-09-09 20:43:06 +0000

    finished! passed

--- my_code.py
+++ my_code.py
@@ -2,31 +2,28 @@
 
 def age_now(x):
     # My age now
-    print(f"I am currently { } years old.\n")
+    print(f"I am currently {x} years old.\n")
 
 def age_1(y):
     # My age next year
-    print(f"Next year I'll be { } years old.\n")
+    print(f"Next year I'll be {y+1} years old.\n")
 
 def age_10(z):
     # My age in 10 years
-    print(f"In 10 years, I'll be { }!\n")
+    print(f"In 10 years, I'll be {z+10}!\n")
 
 def age_50(q):
     # My age in 50 years!
-    print(f"In 50 years, I'll be { }! Wow!\n")
+    print(f"In 50 years, I'll be {q+50}! Wow!\n")
 
 
 
 
 if __name__ == '__main__':
 
-    age = 15    # change this number to your current age
+    age = (int(input("how old are you?")))   # change this number to your current age
 
-    '''
-    after you have this working by defining "age" above, you can comment that line out and add a new line
-    # below it to get the user's age as an input.
-    '''
+ 
 
     age_now(age)   # run function age_now with argument age
     age_1(age)    # run function age_1 with argument age
